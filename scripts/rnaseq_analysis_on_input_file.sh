f766372b7f4118617cf15866ccee95c739b9d59f
Radhika Khetani <khetani.r@gmail.com>
2021-04-02 16:23:53 -0400

    Update rnaseq_analysis_on_input_file.sh

--- scripts/rnaseq_analysis_on_input_file.sh
+++ scripts/rnaseq_analysis_on_input_file.sh
@@ -1,58 +1,80 @@
 #!/bin/bash/
 
-# This script takes a fastq file of RNA-Seq data, runs FastQC and outputs a counts file for it.
-# USAGE: sh rnaseq_analysis_on_allfiles.sh <name of fastq file>
+# This script takes a fastq file of RNA-seq data, runs FastQC, STAR, Qualimap and Salmon.
+# USAGE: sh rnaseq_analysis_on_input_file.sh <name of fastq file>
+
+# change directories to /n/scratch3/ so that all the analysis is stored there.
+cd /n/scratch3/users/r/$USER/rnaseq_hbc-workshop/
 
 # initialize a variable with an intuitive name to store the name of the input fastq file
-
 fq=$1
 
 # grab base of filename for naming outputs
-
-base=`basename $fq .subset.fq`
-echo "Sample name is $base"           
+samplename=`basename $fq .subset.fq`
+echo "Sample name is $samplename"
 
 # specify the number of cores to use
+cores=6
 
-cores=2
-
-# directory with genome reference FASTA and index files + name of the gene annotation file
-
-genome=/groups/hbctraining/unix_workshop_other/reference_STAR/
-gtf=~/unix_workshop/rnaseq/reference_data/chr1-hg19_genes.gtf
+# directory with the genome and transcriptome index files + name of the gene annotation file
+genome=/n/groups/hbctraining/intro_rnaseq_hpc/reference_data_ensembl38/ensembl38_STAR_index
+transcriptome=/n/groups/hbctraining/rna-seq_2019_02/reference_data/salmon_index
+gtf=/n/groups/hbctraining/intro_rnaseq_hpc/reference_data_ensembl38/Homo_sapiens.GRCh38.92.1.gtf
 
 # make all of the output directories
 # The -p option means mkdir will create the whole path if it 
 # does not exist and refrain from complaining if it does exist
-
-mkdir -p ~/unix_workshop/rnaseq/results/fastqc/
-mkdir -p ~/unix_workshop/rnaseq/results/STAR
-mkdir -p ~/unix_workshop/rnaseq/results/counts
+mkdir -p results/fastqc/
+mkdir -p results/STAR/
+mkdir -p results/qualimap/
+mkdir -p results/salmon/
 
 # set up output filenames and locations
+fastqc_out=results/fastqc/
+align_out=results/STAR/${samplename}
+align_out_bam=results/STAR/${samplename}_Aligned.sortedByCoord.out.bam
+qualimap_out=results/qualimap/${samplename}.qualimap
+salmon_out=results/salmon/${samplename}.salmon
+salmon_mappings=results/salmon/${samplename}_salmon.out
 
-fastqc_out=~/unix_workshop/rnaseq/results/fastqc/
-align_out=~/unix_workshop/rnaseq/results/STAR/${base}_
-counts_input_bam=~/unix_workshop/rnaseq/results/STAR/${base}_Aligned.sortedByCoord.out.bam
-counts=~/unix_workshop/rnaseq/results/counts/${base}_featurecounts.txt
-
-# set up the software environment
-
-module load seq/fastqc/0.11.3
-module load seq/STAR/2.5.3a
-module load seq/samtools/1.3
-PATH=/opt/bcbio/centos/bin:$PATH 	# for using featureCounts if not already in $PATH
+# set up the software environment (use version numbers)
+module load fastqc/0.11.3
+module load gcc/6.2.0  
+module load star/2.7.0a
+module load samtools/1.3.1
+module load java/jdk-1.8u112
+module load qualimap/2.2.1
+module load salmon/1.4.0
+unset DISPLAY
 
 echo "Processing file $fq"
 
+echo "Starting QC for $samplename"
+
 # Run FastQC and move output to the appropriate folder
-fastqc $fq
+fastqc -o $fastqc_out $fq
+
 
 # Run STAR
-STAR --runThreadN $cores --genomeDir $genome --readFilesIn $fq --outFileNamePrefix $align_out --outFilterMultimapNmax 10 --outSAMstrandField intronMotif --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes NH HI NM MD AS
+STAR --runThreadN $cores --genomeDir $genome --readFilesIn $fq --outFileNamePrefix $align_out --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes Standard
 
-# Create BAM index
-samtools index $counts_input_bam
+# Run Qualimap
+qualimap rnaseq \
+-outdir $qualimap_out \
+-a proportional \
+-bam $align_out_bam \
+-p strand-specific-reverse \
+-gtf $gtf \
+--java-mem-size=8G
 
-# Count mapped reads
-featureCounts -T $cores -s 2 -a $gtf -o $counts $counts_input_bam
+# Run salmon
+
+echo "Starting Salmon run for $samplename"
+
+salmon quant -i $transcriptome \
+-p $cores \
+-l A \
+-r $fq \
+-o $salmon_out \
+--seqBias \
+--useVBOpt
